a224b476fe785d10697e8bdbad2568dd46659d04
Thomas Collignon <thomas.collignon@external.belighted.com>
2023-02-17 14:59:36 +0100

    init: spring initializr + external DB config

---
+++ src/main/java/com/coli/springsnippets/SpringsnippetsApplication.java
@@ -0,0 +1,13 @@
+package com.coli.springsnippets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsnippetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsnippetsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coli/springsnippets/SpringsnippetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coli.springsnippets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsnippetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
